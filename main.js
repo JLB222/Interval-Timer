5a411aef8c8b7a95d2a75e64225b7a903b8cc2f6
Josh Brown <JLBrown222@gmail.com>
2024-11-21 16:30:18 -0600

    Added rest intervals

--- main.js
+++ main.js
@@ -1,32 +1,27 @@
 let startTime, elapsedTime = 0, timerInterval;
-let userHours, userMinutes, userSeconds, userChosenIntervals, currentInterval = 0, totalUserTime
+let userWorkHours, userWorkMinutes, userWorkSeconds, intervalsCompleted, totalUserWorkTime, totalUserRestTime
+let workInterval = true, restInterval = false;
+let userChosenWorkIntervals, userChosenRestIntervals
+let userRestHours, userRestMinutes, userRestSeconds
 
 const beep = new Audio('sounds/beep-104060.mp3'); // Replace with your sound file path
 
 function chooseInterval() {
-  userChosenIntervals = document.getElementById('userChosenIntervals').value || 0
-  userHours = document.getElementById('userChosenHours').value || 0
-  userMinutes = document.getElementById('userChosenMinutes').value || 0
-  userSeconds = document.getElementById('userChosenSeconds').value || 0
-  totalUserTime = (userHours * 3600000) + (userMinutes * 60000) + (userSeconds * 1000) || undefined  //if all inputs are 0, set to undefined so that elapsedTime is never greater nor less than totalUserTime (so the stopwatch doesn't bug out)
-  currentInterval = 0
-  resetStopwatch()
-  console.log(`${userChosenIntervals} ${userChosenIntervals == 1 ? 'interval': 'intervals'} of ${totalUserTime?.toLocaleString()} milliseconds`)
-}
+  userChosenWorkIntervals = document.getElementById('userChosenWorkIntervals').value || 0
+  userWorkHours = document.getElementById('userChosenWorkHours').value || 0
+  userWorkMinutes = document.getElementById('userChosenWorkMinutes').value || 0
+  userWorkSeconds = document.getElementById('userChosenWorkSeconds').value || 0
+  
+  userRestHours = document.getElementById('userChosenRestHours').value || 0
+  userRestMinutes = document.getElementById('userChosenRestMinutes').value || 0
+  userRestSeconds = document.getElementById('userChosenRestSeconds').value || 0
 
-//remove?
-// function resetInterval() {
-//   userChosenIntervals = 0
-//   userHours = 0
-//   userMinutes = 0
-//   userSeconds = 0
-//   totalUserTime = 0
-//   currentInterval = 0
-//   document.getElementById('userChosenIntervals').value = ""
-//   document.getElementById('userChosenHours').value = ""
-//   document.getElementById('userChosenMinutes').value = ""
-//   document.getElementById('userChosenSeconds').value = ""
-// }
+  totalUserWorkTime = (userWorkHours * 3600000) + (userWorkMinutes * 60000) + (userWorkSeconds * 1000) || undefined  //if all inputs are 0, set to undefined so that elapsedTime is never greater nor less than totalUserWorkTime (so the stopwatch doesn't bug out)
+  totalUserRestTime = (userRestHours * 3600000) + (userRestMinutes * 60000) + (userRestSeconds * 1000) || undefined
+  intervalsCompleted = 0
+  resetStopwatch()
+  console.log(`${userChosenWorkIntervals} ${userChosenWorkIntervals == 1 ? 'interval': 'intervals'} of ${totalUserWorkTime?.toLocaleString()} milliseconds`)
+}
 
 
 function startStopwatch() {
@@ -34,15 +29,25 @@ function startStopwatch() {
   startTime = Date.now() - elapsedTime;  //a value in milliseconds
   timerInterval = setInterval(() => {
     elapsedTime = Date.now() - startTime;
-    if (elapsedTime >= totalUserTime) {  //it's '>=' because '==' often failed to increment, probably due to the passed milliseconds not perfectly lining up with user defined time due to the interval() not firing EXACTLY every 10 milliseconds
-      startTime = Date.now()
-      currentInterval++
-      beep.play()
-      console.log(`Current interval: ${currentInterval}`)
-      if (currentInterval == userChosenIntervals) {
-        resetStopwatch()
-      }
-    };
+    if (workInterval) {
+      if (elapsedTime >= totalUserWorkTime) {  //it's '>=' because '==' often failed to increment, probably due to the passed milliseconds not perfectly lining up with user defined time due to the interval() not firing EXACTLY every 10 milliseconds
+        startTime = Date.now()
+        intervalsCompleted++
+        beep.play()
+        console.log(`Intervals Completed: ${intervalsCompleted}`)
+        totalUserRestTime ? workInterval = false : null  //if user has chosen a rest interval, this will switch the code to 'rest mode' by switching workInterval to false, running the else code that does not increment the interval counter
+        if (intervalsCompleted == userChosenWorkIntervals) {
+          resetStopwatch()
+        }
+      };
+    } else {
+      if (elapsedTime >= totalUserRestTime) {  //it's '>=' because '==' often failed to increment, probably due to the passed milliseconds not perfectly lining up with user defined time due to the interval() not firing EXACTLY every 10 milliseconds
+        startTime = Date.now()
+        beep.play()
+        console.log(`Rest time is over, get back to work!`)
+        workInterval = true  //will end 'rest mode' by making workInterval true again, ensuring the next interval is incremented
+      };
+    }
     updateDisplay();
   }, 40); 
 }
@@ -54,7 +59,8 @@ function stopStopwatch() {
 function resetStopwatch() {
   clearInterval(timerInterval);
   elapsedTime = 0;
-  currentInterval = 0
+  intervalsCompleted = 0
+  workInterval = true
   updateDisplay();
 }
 
@@ -69,10 +75,8 @@ function formatTime(time) {
   const minutes = time.getUTCMinutes().toString().padStart(2, '0');
   const seconds = time.getUTCSeconds().toString().padStart(2, '0');
   const milliseconds = time.getUTCMilliseconds().toString().slice(0, 2).padStart(2, '0');
-  return `${currentInterval}---${hours}:${minutes}:${seconds}.${milliseconds}`;
+  return `${intervalsCompleted}---${hours}:${minutes}:${seconds}.${milliseconds}`;
 }
 
-//let user define how many intervals they wish to do
-//let user define the length of an interval, of potentially different lengths (rest interval, work interval)
-
-//need to find a way to increment intervals.  Current setup bugs out if userTime is ever 0, which we set it to when we reset or confirm.
+//rest beeps that are different from workout beeps, so user knows by sound where they're at
+//final (different) beep for end of workout
